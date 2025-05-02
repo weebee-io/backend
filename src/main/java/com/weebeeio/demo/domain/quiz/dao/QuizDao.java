047b53b4c576cf123607ae2d238d8f686f94b313
lejy <mbc09111@naver.com>
2025-05-02 15:13:28 +0900

    fix: 퀴즈레벨 타입 수정

--- src/main/java/com/weebeeio/demo/domain/quiz/dao/QuizDao.java
+++ src/main/java/com/weebeeio/demo/domain/quiz/dao/QuizDao.java
@@ -18,7 +18,7 @@ public class QuizDao {
     private String subject;          // 퀴즈 주제
 
     @Column(name = "quiz_level")
-    private Integer quizLevel;        // 퀴즈 난이도 (1,3,5)
+    private Integer quizLevel;        // 퀴즈 난이도 (1,3,5) 실버 브론즈 골드
 
     @Column(name = "quiz_answer", nullable = false)
     private String quizAnswer;       // 퀴즈 정답
